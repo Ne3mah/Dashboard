81e8a4ee0370272c40c9e0271e28dedef3d62f56
Neamah Al-Safarjalani <neamah.sfj@gmail.com>
2024-09-15 13:57:26 +0300

    feat: Add configuration for SPA in frappe app.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+		outDir: "../dashboard/public/frontend",
+		emptyOutDir: true,
+		target: "es2015",
+  },
   plugins: [
     vue(),
   ],
